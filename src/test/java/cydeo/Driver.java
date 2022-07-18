98da6b25ec851aadc2edc24f87f90470ca2ec665
Seniye Kaya <seniyeerdinckaya@gmail.com>
2022-07-18 04:11:04 -0400

    trigger practice3

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -48,7 +48,7 @@ public class Driver {
                 case "remote-firefox":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.197.71.67";
+                        String gridAddress = "54.197.71.067";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
